b47c4e3a7add7c5e9f810c10944880127dba827c
caregor <caregor@gmail.com>
2023-07-26 21:36:50 +0500

    Task 5 is done;

--- myfilepackage/__init__.py
+++ myfilepackage/__init__.py
@@ -1,5 +1,7 @@
 __version__ = '1.0.0'
 
+__all__ = ['task1', 'task2', 'task3', 'task4']
+
 
 from .task1 import generate_files_with_extensions
 from .task2 import generate_files_with_extensions_num_files_per_extension
